55f5d97c71f8c0fc453ff397fc44b1abe2be78a5
Marcos Gabriel Barreto <77794251+marbielto@users.noreply.github.com>
2021-03-04 15:37:51 -0300

    Add files via upload

---
+++ juice_maker.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 14:15:01 2021
+
+@author: MGB
+"""
+
+class Juice:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+
+    def __str__(self):
+        return (self.name + ' ('+str(self.capacity)+'L)')
+
+    def __add__(self, other):
+        new_name = self.name+"&"+other.name
+        new_capacity = self.capacity + other.capacity
+        return Juice(new_name, new_capacity)
+    
+a = Juice('Orange', 1.5)
+b = Juice('Apple', 2.0)
+
+result = a + b
+print(result)
