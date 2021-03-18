85495962f05ae289177900ae93c27735413f956b
Marcos Gabriel Barreto <77794251+marbielto@users.noreply.github.com>
2021-03-18 14:38:49 -0300

    Add files via upload

---
+++ the_zen_of_python.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 13:48:30 2021
+
+@author: MGB
+"""
+
+#Which module prints the Zen of Python when imported?
+import this
+#Which of these principles does not appear in the Zen of Python?
+# R: Use recursion where possible
+
