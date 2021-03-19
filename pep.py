8114b84d42119f6edf27fa4dc337eb4511b76242
Marcos Gabriel Barreto <77794251+marbielto@users.noreply.github.com>
2021-03-19 17:33:00 -0300

    Add files via upload

---
+++ pep.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 19 17:27:01 2021
+
+@author: MGB
+"""
+
+"""
+- modules should have short, all-lowercase names;
+- class names should be in the CapWords style;
+- most variables and function names should be lowercase_with_underscores;
+- constants (variables that never change value) should be CAPS_WITH_UNDERSCORES;
+- names that would clash with Python keywords (such as 'class' or 'if') should have a trailing underscore.
+"""
+
+#Which choice is PEP 8-compliant as the name of a class?
+# R: MyClassName
+
+#Other PEP 8 suggestions include the following:
+"""
+- lines shouldn't be longer than 80 characters;
+- 'from module import *' should be avoided;
+- there should only be one statement per line.
+"""
