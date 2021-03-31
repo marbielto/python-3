a37af626f236435008b60b72cc50b36b45ecf619
Marcos Gabriel Barreto <77794251+marbielto@users.noreply.github.com>
2021-03-30 23:18:13 -0300

    Add files via upload

---
+++ ternary_operator.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 22:39:54 2021
+
+@author: MGB
+"""
+
+a = 7
+b = 1 if a>= 5 else 42
+print(b)
+
+status = 1
+msg = "Logout" if status == 1 else "Login"
+print(msg)
